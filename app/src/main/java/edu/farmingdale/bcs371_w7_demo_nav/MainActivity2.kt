3cc604786da9b126e66d589b62624767eae5eb95
dmondal06 <deepa.mondal321@gmail.com>
2024-10-15 14:28:39 -0400

    Completed todo 1

--- app/src/main/java/edu/farmingdale/bcs371_w7_demo_nav/MainActivity2.kt
+++ app/src/main/java/edu/farmingdale/bcs371_w7_demo_nav/MainActivity2.kt
@@ -68,7 +68,8 @@ fun BasicOperations(name: String, modifier: Modifier = Modifier) {
 
         Button( onClick = {
             val newInt = Intent(Intent.ACTION_VIEW)
-            // ToDo 1: create implicit intent to open a web page or call a phone number
+            // ToDo 1: Done create implicit intent to open a web page or call a phone number
+            newInt.setData(Uri.parse("tel:+9999999999"))
             context.startActivity(newInt)
         },
             modifier= Modifier.padding(start = 40.dp, end = 40.dp)) {
@@ -79,6 +80,7 @@ fun BasicOperations(name: String, modifier: Modifier = Modifier) {
         HorizontalDivider(thickness = DividerDefaults.Thickness)
 
         Button( onClick = {
+
             // ToDo 2: create explicit intent to open a new activity
             
         },
